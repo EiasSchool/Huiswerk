ef657748c5bc977855eae3565a41ae5fa715a611
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-08 18:00:27 +0200

    H.W2.O1 klaar

---
+++ Week 2/Opdracht 2/ HP1.W2.OB.1.py
@@ -0,0 +1,10 @@
+pi = 3.14159
+
+straal = float(input("Voer de straal van de cirkel in:\n"))
+
+oppervlakte = straal * straal * pi
+
+omtrek = 2 * pi * straal
+
+print(f"De oppervlakte van een cirkel met straal {straal} is {oppervlakte}")
+print(f"De omtrek van de cirkel met straal {straal} is {omtrek}")
