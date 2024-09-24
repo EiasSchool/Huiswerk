d45e8f4552609ef7fd41b4d84db157bd60ab2726
Eias <51035404+1Eias@users.noreply.github.com>
2024-09-24 18:16:37 +0200

    H.W4.O3

---
+++ Week 4/Opdracht 3/Swap variables.py
@@ -0,0 +1,14 @@
+a = 75
+b = 125
+print("a =", a, "en b =", b)
+a += b # dit is hetzelfde als: a = a + b
+
+# voeg hieronder 2 regels code toe
+
+b = a - b # b = 200 - 125 = 75
+a = a - b # a = 200 - 75 = 125
+
+
+# en print
+print("a =", a, "en b =", b)
+
