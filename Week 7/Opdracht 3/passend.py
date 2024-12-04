e9df79ee311b3dc8003744bb192a1829d44c3803
Eias <51035404+1Eias@users.noreply.github.com>
2024-12-04 11:37:35 +0100

    HP1.W7.O3

---
+++ Week 7/Opdracht 3/passend.py
@@ -0,0 +1,8 @@
+number = 13
+maxNumber = 625
+
+divide = maxNumber // number
+remaining = maxNumber % number
+
+print(f'{number} fits {divide} times in {maxNumber}')
+print(f'There is {remaining} left over')
